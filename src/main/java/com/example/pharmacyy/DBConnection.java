7556ceebf56be231650e564d1709fa85ba5804be
Hiaines <93349408+Hiaines@users.noreply.github.com>
2022-12-28 21:08:55 +0100

    Update DBConnection.java

--- src/main/java/com/example/pharmacyy/DBConnection.java
+++ src/main/java/com/example/pharmacyy/DBConnection.java
@@ -1,2 +1,31 @@
-package com.example.pharmacyy;public class DBconnection {
+package com.example.pharmacyy;
+
+import javafx.scene.control.SplitPane;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+static String user="root";
+static String password="ines";
+static String url="jdbc:mysql://localhost/Products";
+static String driver="com.mysql.cj.jdbc.Driver";
+
+public static Connection getCon(){
+    Connection con=null;
+    try {
+        Class.forName(driver);
+        try {
+            con= DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    } catch (ClassNotFoundException e) {
+        throw new RuntimeException(e);
+    }
+
+    return con;
+}
 }
