febbdaddcb37f57168da8c46673b54f80a343965
Hiaines <93349408+Hiaines@users.noreply.github.com>
2022-12-28 21:10:47 +0100

    Update App.java

--- src/main/java/com/example/pharmacyy/App.java
+++ src/main/java/com/example/pharmacyy/App.java
@@ -1,2 +1,22 @@
-package com.example.pharmacyy;public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Products.fxml")));
+        Scene scene=new Scene(parent);
+        stage.setTitle("product");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
