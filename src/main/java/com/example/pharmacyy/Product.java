afe656dd7261b70fb8a81a6429ecde9dea085389
Hiaines <93349408+Hiaines@users.noreply.github.com>
2022-12-28 21:11:13 +0100

    Update Product.java

--- src/main/java/com/example/pharmacyy/Product.java
+++ src/main/java/com/example/pharmacyy/Product.java
@@ -1,2 +1,60 @@
-package com.example.pharmacyy;public class Product {
+package com.example.pharmacyy;
+
+import java.util.Date;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Double price;
+    private Date EXP;
+    private Integer Qte;
+    private String company;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice(double price) {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getEXP() {
+        return EXP;
+    }
+
+    public void setEXP(Date EXP) {
+        this.EXP = EXP;
+    }
+
+    public Integer getQte() {
+        return Qte;
+    }
+
+    public void setQte(Integer qte) {
+        Qte = qte;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
