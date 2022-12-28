4e963d7cfe91e9c8a45144093c41421032ab05a1
Hiaines <93349408+Hiaines@users.noreply.github.com>
2022-12-28 21:20:47 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.pharmacyy {
     requires javafx.controls;
     requires javafx.fxml;
+    requires mysql.connector.java;
+    requires java.sql;
 
 
     opens com.example.pharmacyy to javafx.fxml;
